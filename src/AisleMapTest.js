942c13a8aaa4fa6e69476129b0188c84c2a6949e
Richard Artoul <richardartoul@gmail.com>
2015-06-10 19:38:50 -0700

    [Feature] Added aisles the html5 animation

--- src/AisleMapTest.js
+++ src/AisleMapTest.js
@@ -4,12 +4,39 @@ var context = canvas.getContext('2d');
 context.beginPath();
 context.moveTo(500 + testMap.path[0].x * 40, 500 + testMap.path[0].y * 40);
 
-// line 1
-for (var i = 0; i < testMap.path.length; i++) {
-	console.log("x:", testMap.path[i].x*40+500, "y:", testMap.path[i].y*40+500)
-  context.lineTo(testMap.path[i].x*40+500, -testMap.path[i].y*40+500);
+//finds the next point at which the direction changes
+var findDirectionChange = function() {
 }
+	
+var drawPath = function() {
+	var counter = 0;
 
-context.lineWidth = 5;
-context.strokeStyle = 'blue';
-context.stroke();
+	context.beginPath();
+	context.moveTo(500 + testMap.path[0].x * 40, 500 + testMap.path[0].y * 40);
+	var animate = function() {
+	  	context.lineTo(testMap.path[counter].x*40+500, -testMap.path[counter].y*40+500);
+
+		context.lineWidth = 5;
+		context.strokeStyle = 'blue';
+		context.stroke();
+		counter++;
+		if (counter < testMap.path.length) {
+			setTimeout(animate, 100);
+		}
+	}
+	animate();	
+}	
+
+var drawAisles = function(numAisles) {
+	for (var i = 0; i < testMap.numAisles; i++) {
+		context.beginPath();
+		context.moveTo(480+40*i, 500);
+		context.lineTo(480+40*i, 100);
+		context.lineWidth = 2;
+		context.strokeStyle = 'black';
+		context.stroke();
+	}
+} 
+
+drawAisles();
+drawPath();
