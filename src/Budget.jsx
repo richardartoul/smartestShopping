5e5286334ddabdeaa93db4ff0cd0dbd6f9f19efa
Kuldeep <ksdhanjal@yahoo.com>
2015-06-11 19:47:01 -0700

    [feature] Update Budget works  -WIP

--- src/Budget.jsx
+++ src/Budget.jsx
@@ -19,12 +19,57 @@ var Budget = Eventful.createClass({
       });
     }
   },
+  
+  updateBudget: function(e) {
+      console.log("invoked updateBudget in jsx " + e);
+
+    e.preventDefault();
+    var updatedBudget = e.target.updateBudgetInput.value;
+    e.target.updateBudgetInput.value = '';
+
+    this.emit('update-budget', { name: updatedBudget });
+
+  },
+
+
 
   render: function() {
     return (
-      <div id="home">
+      <div id="budget">
         <NavToggle />
-        <h1> will set up budget here </h1>
+
+      <div className="row">
+        <div className="col-md-4"></div>
+        <div className="col-md-4">
+          <div className="ibox float-e-margins" id="list-border">
+            <div className="ibox-title">
+              <h5>Budget Management and Spending</h5>
+            </div>
+            <div className="ibox-content">
+
+              <div className="row">
+                <h2 className="setBudget">Current Budget: ${this.props.budget}</h2>
+                <h2 className="remainingBudget">Remaining Balance: ${this.props.remainingBudget}</h2>
+              </div>
+
+              <div className="row">
+                <div className="list">
+                  <div className='new-item-input'>
+                    <form name="update-budget-form" onSubmit={this.updateBudget}>
+                      <input className='new-item-input' type="text" ref="updateBudgetInput" name="updateBudgetInput" placeholder="Input New Budget"/>
+                      <input className='btn btn-sm btn-primary add-item-button' type="submit" value="Update Budget"/>
+                    </form>
+                </div>
+               </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4"></div>
+      </div>
+
+
       </div>
     );
   }
